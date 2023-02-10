084fd56a618ecf77eb7d8333aa8992cd9cf3105e
spaenleh <spaenleh@gmail.com>
2023-02-10 16:46:31 +0100

    chore: add missing types

--- src/frontend/types.ts
+++ src/frontend/types.ts
@@ -12,9 +12,13 @@ import {
   Member,
   MemberExtra,
   MentionStatus,
+  PermissionLevel,
   S3FileItemType,
   ShortcutItemType,
 } from 'src/index';
+
+// alias type for uuid v4
+type UUID = string;
 
 /**
  * Convenience type to convert nested objects to deeply immutable objects
@@ -90,16 +94,21 @@ export type MemberMentionsRecord = ImmutableCast<MemberMentions>;
 
 export type MessageBodyType = { message: string; mentions?: string[] };
 
-export type PartialNewChatMessage = {
-  chatId: string;
-  body: MessageBodyType;
+export type Category = {
+  id: UUID;
+  name: string;
+  type: UUID;
 };
 
-export type PartialChatMessage = {
-  chatId: string;
-  messageId: string;
-  body?: MessageBodyType;
+export type CategoryRecord = RecordOf<Category>;
+
+// todo: Do we use this or the one with the `type` prop ??
+export type CategoryType = {
+  id: UUID;
+  name: string;
 };
+
+export type CategoryTypeRecord = RecordOf<CategoryType>;
 
 export type ChatMessage = {
   id: string;
@@ -118,3 +127,171 @@ export type ItemChat = {
 };
 
 export type ItemChatRecord = ImmutableCast<ItemChat>;
+
+export type ItemTag = {
+  id: UUID;
+  itemPath: string;
+  tagId: UUID;
+  createdAt: string;
+  creator: string;
+};
+
+export type ItemTagRecord = RecordOf<ItemTag>;
+
+export type Flag = {
+  id: UUID;
+  name: string;
+};
+
+export type FlagRecord = RecordOf<Flag>;
+
+export type Invitation = {
+  id: UUID;
+  email: string;
+  permission?: PermissionLevel;
+  name?: string;
+  creator: UUID;
+  itemPath: string;
+};
+
+export type InvitationRecord = RecordOf<Invitation>;
+
+export type ItemCategory = {
+  id: UUID;
+  itemId: UUID;
+  categoryId: UUID;
+  createdAt: string;
+  creator: string;
+};
+
+export type ItemCategoryRecord = RecordOf<ItemCategory>;
+
+export enum ITEM_LOGIN_SCHEMAS {
+  USERNAME = 'username',
+  USERNAME_AND_PASSWORD = 'username+password',
+}
+
+export type ItemLogin = {
+  loginSchema: ITEM_LOGIN_SCHEMAS;
+};
+
+export type ItemLoginRecord = RecordOf<ItemLogin>;
+
+// type of the exported chat message
+// contains the additional "creatorName" key with the plain text name of the user
+export type ExportedChatMessage = {
+  id: string;
+  chatId: string;
+  creator: string;
+  creatorName: string;
+  createdAt: string;
+  updatedAt: string;
+  body: string;
+};
+
+export type ExportedChatMessageRecord = RecordOf<ExportedChatMessage>;
+
+export type ExportedItemChat = {
+  id: string;
+  messages: List<ExportedChatMessageRecord>;
+};
+
+export type ExportedItemChatRecord = RecordOf<ExportedItemChat>;
+
+// a combined record from item-validation, item-validation-review, item-validation-process
+export type FullValidationRecord = {
+  id: string;
+  itemId: string;
+  reviewStatusId: string;
+  validationStatusId: string;
+  validationResult: string;
+  process: string;
+  createdAt: string;
+};
+
+export type FullValidationRecordRecord = RecordOf<FullValidationRecord>;
+
+export type ItemValidationAndReview = {
+  itemValidationId: string;
+  reviewStatusId: string;
+  reviewReason: string;
+  createdAt: string;
+};
+
+export type ItemValidationAndReviewRecord = RecordOf<ItemValidationAndReview>;
+
+export type ItemValidationGroup = {
+  id: string;
+  itemId: string;
+  itemValidationId: string;
+  processId: string;
+  statusId: string;
+  result: string;
+  updatedAt: string;
+  createdAt: string;
+};
+
+export type ItemValidationGroupRecord = RecordOf<ItemValidationGroup>;
+
+export type Status = {
+  id: string;
+  name: string;
+};
+
+export type StatusRecord = RecordOf<Status>;
+
+export interface Action {
+  id: string;
+  itemId: UUID;
+  memberId: UUID;
+}
+
+export type ActionRecord = RecordOf<Action>;
+export type ActionMetadata = {
+  numActionsRetrieved: number;
+  requestedSampleSize: number;
+};
+export type ActionMetadataRecord = RecordOf<{
+  numActionsRetrieved: number;
+  requestedSampleSize: number;
+}>;
+
+export interface ActionData {
+  actions: List<ActionRecord>;
+  descendants: List<ItemRecord>;
+  item: ItemRecord;
+  itemMemberships: List<ItemMembershipRecord>;
+  members: List<MemberRecord>;
+  metadata: ActionMetadataRecord;
+}
+
+export type ActionDataRecord = RecordOf<ActionData>;
+
+export type Password = string;
+export type NewInvitation = Pick<Invitation, 'email' & 'permission'> &
+  Partial<Invitation>;
+
+export type ItemLike = {
+  id: UUID;
+  itemId: UUID;
+  memberId: string;
+  createdAt: string;
+};
+
+export type ItemLikeRecord = RecordOf<ItemLike>;
+
+export type App = {
+  name: string;
+  url: string;
+  description: string;
+  extra: any;
+};
+
+export type AppRecord = RecordOf<App>;
+
+export type Tag = {
+  id: UUID;
+  name: string;
+};
+
+export type TagRecord = RecordOf<Tag>;
