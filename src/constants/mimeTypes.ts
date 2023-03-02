f2b544582cf2c9854baf5581e315e16a6d049062
spaenleh <spaenleh@gmail.com>
2023-03-02 16:08:05 +0100

    chore: improve how mimetypes are stored and can be accessed with keys

--- src/constants/mimeTypes.ts
+++ src/constants/mimeTypes.ts
@@ -1,13 +1,57 @@
+const JPEG = 'image/jpeg';
+const JPG = 'image/jpg';
+const PNG = 'image/png';
+const GIF = 'image/gif';
+const SVG = 'image/svg+xml';
+
+const IMAGE = {
+  JPG,
+  JPEG,
+  PNG,
+  GIF,
+  SVG,
+};
+
+const MP4 = 'video/mp4';
+// https://stackoverflow.com/questions/15277147/m4v-mimetype-video-mp4-or-video-m4v
+const MP4_Apple = 'video/x-m4v';
+const OGG = 'video/ogg';
+const QUICKTIME = 'video/quicktime';
+const WEBM = 'video/webm';
+
+const VIDEO = {
+  MP4,
+  MP4_Apple,
+  OGG,
+  QUICKTIME,
+  WEBM,
+};
+
+const MPEG = 'audio/mpeg';
+const MP3 = 'audio/mp3';
+const WAV = 'audio/wav';
+const WAV_Chrome = 'audio/x-wav';
+
+const AUDIO = {
+  MPEG,
+  MP3,
+  WAV,
+  WAV_Chrome,
+};
+
+const PDF = 'application/pdf';
+
+const ZIP = 'application/zip';
+
 export const MIME_TYPES = {
-  IMAGE: ['image/png', 'image/jpg', 'image/gif', 'image/jpeg', 'image/svg+xml'],
-  VIDEO: [
-    'video/mp4',
-    'video/x-m4v',
-    'video/ogg',
-    'video/quicktime',
-    'video/webm',
-  ],
-  AUDIO: ['audio/mpeg', 'audio/mp3', 'audio/wav', 'audio/x-wav'],
-  PDF: ['application/pdf'],
-  ZIP: ['application/zip'],
+  IMAGE,
+  VIDEO,
+  AUDIO,
+  PDF,
+  ZIP,
+  isImage: (mimetype: string) => Object.values(IMAGE).includes(mimetype),
+  isAudio: (mimetype: string) => Object.values(AUDIO).includes(mimetype),
+  isVideo: (mimetype: string) => Object.values(VIDEO).includes(mimetype),
+  isPdf: (mimetype: string) => [PDF].includes(mimetype),
+  isZip: (mimetype: string) => [ZIP].includes(mimetype),
 } as const;
