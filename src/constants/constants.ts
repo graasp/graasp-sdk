1b9c9855964719387c69c012546327abc3f5f8f4
spaenleh <spaenleh@gmail.com>
2023-04-14 15:42:17 +0200

    fix: remove DEFAULT_LINK_SHOW_BUTTON and put it in ui

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,5 +2,3 @@
 export const SESSION_COOKIE_EXPIRATION_DURATION_MS = 7 * 24 * 60 * 60 * 1000; // 7 days
 
 export const IFRAME_RESIZE_HEIGHT_COOKIE_EXPIRATION_DAYS = 365; // 365 days
-
-export const DEFAULT_LINK_SHOW_BUTTON = true;
