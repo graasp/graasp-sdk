08b692c9b76d4b6fe3e39db270f1b764dd82dbaa
LinaYahya <linaebe0@gmail.com>
2023-10-17 10:22:46 +0200

    fix: triggers types to camelcase

--- src/constants/triggers.ts
+++ src/constants/triggers.ts
@@ -1,6 +1,6 @@
 export enum Triggers {
-  COLLECTION_VIEW = 'collection-view',
-  ITEM_VIEW = 'item-view',
-  LINK_OPEN = 'link-open',
-  ITEM_DOWNLOAD = 'item-download',
+  CollectionView = 'collection-view',
+  ItemView = 'item-view',
+  LinkOpen = 'link-open',
+  ItemDownload = 'item-download',
 }
