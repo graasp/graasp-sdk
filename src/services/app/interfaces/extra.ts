c37f1140a344b62cf68ff681d6232443799c9446
spaenleh <spaenleh@gmail.com>
2023-10-20 15:34:52 +0200

    fix: use unknown instead of object

--- src/services/app/interfaces/extra.ts
+++ src/services/app/interfaces/extra.ts
@@ -3,7 +3,7 @@ import { ItemType } from '../../../constants';
 export type AppItemExtraProperties = {
   url: string;
   // todo: there currently is nothing stored in the settings. this might change later
-  settings?: object;
+  settings?: unknown;
 };
 
 export interface AppItemExtra {
