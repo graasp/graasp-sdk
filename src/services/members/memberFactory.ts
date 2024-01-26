58027cf5a878d180489a320b3d1cd934cb6b7ea8
kim <kim.phanhoang@epfl.ch>
2024-01-26 13:42:23 +0100

    refactor: accept values

--- src/services/members/memberFactory.ts
+++ src/services/members/memberFactory.ts
@@ -1,17 +1,18 @@
 import { CompleteMember, MemberType } from '.';
 import { faker } from '@faker-js/faker';
 
-export const MemberFactory = ()=> <CompleteMember>({
+export const MemberFactory = (
+  m: Partial<CompleteMember> = {},
+): CompleteMember => ({
   id: faker.string.uuid(),
-  name:  faker.person.fullName(),
+  name: faker.person.fullName(),
   email: faker.internet.email(),
-  createdAt:  faker.date.anytime().toISOString(),
-  updatedAt:  faker.date.anytime().toISOString(),
-  extra: 
-    faker.helpers.arrayElement([
-      { lang: faker.helpers.arrayElement(['en', 'fr', 'de']) },
-      {},
-    ])
-  ,
+  createdAt: faker.date.anytime().toISOString(),
+  updatedAt: faker.date.anytime().toISOString(),
+  extra: faker.helpers.arrayElement([
+    { lang: faker.helpers.arrayElement(['en', 'fr', 'de']) },
+    {},
+  ]),
   type: MemberType.Individual, // todo
+  ...m,
 });
