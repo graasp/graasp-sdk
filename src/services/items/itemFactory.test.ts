a56663e61e8d38ee7f171fc46ca5aae97406ded5
kim <kim.phanhoang@epfl.ch>
2024-01-26 14:22:49 +0100

    feat: add item factory

---
+++ src/services/items/itemFactory.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from 'vitest';
+
+import { ItemFactory } from '.';
+
+describe('ItemFactory', () => {
+  it('Returns correct path for id', () => {
+    const item = ItemFactory({ id: '2d11f9d9-fbbe-4e0a-9a13-0999ca20bebc' });
+    expect(item.path).toEqual('2d11f9d9_fbbe_4e0a_9a13_0999ca20bebc');
+  });
+  it.only('Returns correct path for parentItem', () => {
+    const item1 = ItemFactory({
+      parentItem: ItemFactory({ id: '3d11f9d9-fbbe-4e0a-9a13-0999ca20bebc' }),
+    });
+    expect(item1.path).toContain('3d11f9d9_fbbe_4e0a_9a13_0999ca20bebc');
+    const item2 = ItemFactory({
+      id: '2d11f9d9_fbbe_4e0a_9a13_0999ca20bebc',
+      parentItem: ItemFactory({ id: '3d11f9d9-fbbe-4e0a-9a13-0999ca20bebc' }),
+    });
+    expect(item2.path).toEqual(
+      '3d11f9d9_fbbe_4e0a_9a13_0999ca20bebc.2d11f9d9_fbbe_4e0a_9a13_0999ca20bebc',
+    );
+  });
+});
