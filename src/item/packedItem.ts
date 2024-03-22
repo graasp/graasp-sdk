df0ec74373227068d8502d456b81783e883ebd90
kim <kim.phanhoang@epfl.ch>
2024-03-22 09:21:17 +0100

    feat: add packed factories

--- src/item/packedItem.ts
+++ src/item/packedItem.ts
@@ -2,10 +2,13 @@ import { ItemMembership } from '../itemMembership/itemMembership.js';
 import { DiscriminatedItem } from './item.js';
 import { ItemSettings } from './itemSettings.js';
 
+export type PackedInformation = {
+  permission: ItemMembership['permission'];
+};
+
 /**
  * This type is used to define an item with more data such as:
  * - permission
  */
-export type PackedItem<S = ItemSettings> = DiscriminatedItem<S> & {
-  permission: ItemMembership['permission'];
-};
+export type PackedItem<S = ItemSettings> = DiscriminatedItem<S> &
+  PackedInformation;
