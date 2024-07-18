1715e477bd64f1953ab2ded35f919ee749ad042b
Jeremy Defferrard <jeremy.deff@gmail.com>
2024-07-18 13:49:26 +0200

    feat: guest and account member type

--- src/member/factory.ts
+++ src/member/factory.ts
@@ -1,21 +1,40 @@
-import { CompleteMember, MemberType } from './member.js';
+import {
+  BaseAccount,
+  CompleteGuest,
+  CompleteMember,
+  MemberType,
+} from './member.js';
 import { faker } from '@faker-js/faker';
+
+function baseAccountFactory<T extends MemberType>(
+  baseAccount: Partial<BaseAccount> & { type: T },
+): BaseAccount & { type: T } {
+  return {
+    id: faker.string.uuid(),
+    name: faker.person.fullName(),
+    createdAt: faker.date.anytime().toISOString(),
+    updatedAt: faker.date.anytime().toISOString(),
+    ...baseAccount,
+  };
+}
 
 export const MemberFactory = (
   m: Partial<CompleteMember> = {},
 ): CompleteMember => ({
-  id: faker.string.uuid(),
-  name: faker.person.fullName(),
   email: faker.internet.email(),
-  createdAt: faker.date.anytime().toISOString(),
-  updatedAt: faker.date.anytime().toISOString(),
   extra: faker.helpers.arrayElement([
     { lang: faker.helpers.arrayElement(['en', 'fr', 'de']) },
     {},
   ]),
-  // todo: handle other member type when useful in backend
-  type: MemberType.Individual,
   enableSaveActions: m.enableSaveActions ?? true,
   isValidated: m.isValidated ?? true,
+  ...baseAccountFactory({ type: MemberType.Individual }),
   ...m,
+});
+
+export const GuestFactory = (
+  g: Partial<CompleteGuest> & Pick<CompleteGuest, 'itemLoginSchema'>,
+): CompleteGuest => ({
+  ...baseAccountFactory({ type: MemberType.Guest }),
+  ...g,
 });
